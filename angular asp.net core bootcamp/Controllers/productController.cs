c5d2a4aa3e539027975bf65ebe52479d1bfb1894
GRAPHICS <GRAPHICS@AKASH>
2020-01-18 17:54:56 +0530

    a

--- angular asp.net core bootcamp/Controllers/productController.cs
+++ angular asp.net core bootcamp/Controllers/productController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using angular_asp.net_core_bootcamp.model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace angular_asp.net_core_bootcamp.Controllers
