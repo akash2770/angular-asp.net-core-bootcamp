22216cc2869172b80cc648d5c9627a9856afc517
GRAPHICS <GRAPHICS@AKASH>
2020-01-18 18:11:00 +0530

    G

--- angular asp.net core bootcamp/model/tbl_product_master.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace angular_asp.net_core_bootcamp.model
-{
-    internal class tbl_product_master
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-        public int qty { get; set; }
-    }
-}
