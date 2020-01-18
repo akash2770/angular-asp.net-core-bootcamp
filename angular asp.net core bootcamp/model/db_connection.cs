cf81c81fbc0be488188d3b6dde0f9f83930c09fe
GRAPHICS <GRAPHICS@AKASH>
2020-01-18 17:02:18 +0530

    P

---
+++ angular asp.net core bootcamp/model/db_connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angular_asp.net_core_bootcamp.model
+{
+    public class db_connection
+    {
+    }
+}
