17b6e4d6da06ea134cb101f78405b55d71fe848c
panth <panth@PANTH-PC>
2020-01-18 21:57:07 +0530

    h

--- angular asp.net core bootcamp/Controllers/productController.cs
+++ angular asp.net core bootcamp/Controllers/productController.cs
@@ -30,8 +30,7 @@ namespace angular_asp.net_core_bootcamp.Controllers
         [HttpGet("{id}")]
         public IActionResult view(int id)
         {
-            //asas
-            //as
+            //new
             var p_view = con.tbl_product_master.FirstOrDefault(x => x.id == id);
             return Ok(p_view);
         }
