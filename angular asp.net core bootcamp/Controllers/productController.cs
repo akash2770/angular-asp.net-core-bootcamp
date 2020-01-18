3d724e4d03481d17e924a626f081241600047417
GRAPHICS <GRAPHICS@AKASH>
2020-01-18 17:51:23 +0530

    akash

---
+++ angular asp.net core bootcamp/Controllers/productController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using angular_asp.net_core_bootcamp.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace angular_asp.net_core_bootcamp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class productController : ControllerBase
+    {
+        private readonly db_connection con;
+        public productController(db_connection con)
+        {
+            this.con = con;
+        }
+
+        [HttpGet]
+        public IActionResult list()
+        {
+            var product = con.tbl_product_master.ToList();
+            return Ok(product);
+        }
+
+    }
+}
