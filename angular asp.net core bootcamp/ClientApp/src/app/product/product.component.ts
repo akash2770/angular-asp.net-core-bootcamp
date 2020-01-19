5af8da81970b92052c06b04a5f356c0f3f1a930c
panth <panth@PANTH-PC>
2020-01-19 22:35:51 +0530

    h

--- angular asp.net core bootcamp/ClientApp/src/app/product/product.component.ts
+++ angular asp.net core bootcamp/ClientApp/src/app/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './product.service';
 import { Product } from './product.model';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -12,15 +12,40 @@ export class ProductComponent implements OnInit {
 
   private product: Product = new Product();
 
-  constructor(private productService: ProductService, private route: Router) { }
+  constructor(private productService: ProductService,
+    private route: Router,
+    private router: ActivatedRoute) { }
+
+  private url_id: number;
+  private check_add_update = false;
+    
 
   ngOnInit() {
+    this.url_id = this.router.snapshot.params['id']
+    this.find();
   }
 
-  addProduct() {
-    this.productService.add_product(this.product).subscribe(res => {
-      this.route.navigate(['product']);
-    });
+  find() {
+    this.productService.view_product(this.url_id).subscribe(
+      res => {
+        this.product.id = res['id'];
+        this.product.title = res['title'];
+        this.product.qty = res['qty'];
+        this.check_add_update = true;
+      }
+    );
+  }
+
+  productSubmit() {
+    if (this.check_add_update === false) {
+      this.productService.add_product(this.product).subscribe(res => {
+        this.route.navigate(['product']);
+      });
+    } else {
+      this.productService.update_product(this.product).subscribe(res => {
+        this.route.navigate(['product']);
+      });
+    }
   }
 
 
--- angular asp.net core bootcamp/ClientApp/src/app/product/product.service.ts
+++ angular asp.net core bootcamp/ClientApp/src/app/product/product.service.ts
@@ -15,8 +15,16 @@ export class ProductService {
     return this.http.get<Product[]>('https://localhost:44352/api/product/list');
   }
 
+  view_product(id: number) {
+    return this.http.get('https://localhost:44352/api/product/view/' + id);
+  }
+
   add_product(product: Product) {
     return this.http.post('https://localhost:44352/api/product/add', product);
+  }
+
+  update_product(product: Product) {
+    return this.http.put('https://localhost:44352/api/product/update', product);
   }
 
   delete_product(id: number) {
